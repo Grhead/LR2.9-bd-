dbc428cb60374b0265cd089b3cfa6ed89e6f7087
Grenka <64643423+Grhead@users.noreply.github.com>
2022-04-07 09:47:06 +0700

    Update User.cs

--- LR2.9(bd)/User.cs
+++ LR2.9(bd)/User.cs
@@ -13,7 +13,7 @@ namespace LR2._9_bd_
         {
             Tasks = new HashSet<Taskk>();
         }
-        public int Userid { get; set; }
+        public int UserId { get; set; }
         public string F_Name { get; set; }
         public string S_Name { get; set; }
         public string L_Name { get; set; }
