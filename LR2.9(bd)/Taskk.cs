9ca9454f52b62f78685160f904181d9f7fd76b0d
Grenka <64643423+Grhead@users.noreply.github.com>
2022-04-07 09:46:42 +0700

    Update Taskk.cs

--- LR2.9(bd)/Taskk.cs
+++ LR2.9(bd)/Taskk.cs
@@ -6,15 +6,15 @@ public class Taskk
     {
 
     }
-    public int Taskid { get; set; }
+    public int TaskId { get; set; }
     public string Name_Task { get; set; }
     public string Description_Task { get; set; }
     public DateTime Date_Pub { get; set; }
 
-    public int CreatorID { get; set; }
+    public int UsersID { get; set; }
 
     //public int AcceptorID { get; set; }
-    public int Statusid { get; set; }
+    public int StatusId { get; set; }
 
     public virtual Status Statuses { get; set; }
     public virtual User Users { get; set; }
